c4911986057d7a321e4dc3e61360f1c6366e694a
Ivan Chutkan <alexandrbozhkov1977@gmail.com>
2020-04-02 11:09:12 +0200

    callBack module done

--- scripts/script.js
+++ scripts/script.js
@@ -3,46 +3,44 @@ window.addEventListener('DOMContentLoaded', () => {
     'use strict';
 
     const callBack = () => {
-        const header = document.querySelector('header'),
-        btnCall = document.querySelectorAll('.call-btn');
-        console.log(header);
-        console.log(btnCall);
+        
+        const popupCall = document.querySelector('.popup-call'),
+        popupContent = document.querySelector('.popup-content');
+            
 
+        const showPopupCall = () => {
+             popupCall.style.display = 'block';
+            let top = -70;
+            let animPopup = () => {
+                top += 5;
+                 popupContent.style.top = top + '%';
+                    if(top <= 20) {
+                        requestAnimationFrame(animPopup);
+                    }      
+            };
+            animPopup();
+        };
 
-
-
+        document.addEventListener('click', event => {
+        event.preventDefault();
+        let target = event.target;
+        if(target.matches('.call-btn')) {
+            showPopupCall(); 
+            }
+        if(target.matches('.popup-close')) {
+            console.log(target);
+            popupCall.style.display = 'none';
+            } else {
+                target = target.matches('.popup-call');
+                if(target) {
+                    popupCall.style.display = 'none';
+                    console.log(target);
+                }
+            }
+        });
     };
 
     callBack();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
