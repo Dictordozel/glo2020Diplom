79f0171aa7e5a9ec42dc971d9244a1832fcbacfb
Ivan Chutkan <alexandrbozhkov1977@gmail.com>
2020-04-08 17:58:31 +0200

    Make calculator

--- scripts/septConstructor.js
+++ scripts/septConstructor.js
@@ -60,9 +60,17 @@ window.addEventListener('DOMContentLoaded', () => {
 
 
             const myonoffswitch = document.getElementById('myonoffswitch'),
+            onoffswitcCheckbox = document.querySelectorAll('.onoffswitch-checkbox'),
             myonoffswitchTwo = document.getElementById('myonoffswitch-two'),
             selectBox = document.querySelectorAll('.select-box'),
-            titleText = document.querySelectorAll('.title-text');
+            titleText = document.querySelectorAll('.title-text'),
+            calcResult = document.getElementById('calc-result'),
+            collapseTwo = document.getElementById('collapseTwo'),
+            formControl = collapseTwo.querySelectorAll('.form-control'),
+            accordion = document.getElementById('accordion');
+
+            
+
            
             
 
@@ -71,58 +79,156 @@ window.addEventListener('DOMContentLoaded', () => {
 
                 constructor() {
                     
-                    this.diameter = 0;
                     this.ringsCount = 0;
-                    this.bottom = true;
                     this.distanse = 0;
-
+                    this.bodyCount = 0;
+                    this.bodyBottom = 0;
+                    this.totalCount = 0;
+                    this.bodyDiameter = 0;
 
                 }
 
-                run() {
+            run() {
+               
+            titleText[1].style.display = 'none';
+            selectBox[2].style.display = 'none';
+            selectBox[3].style.display = 'none';
 
-                titleText[1].style.display = 'none';
-                selectBox[2].style.display = 'none';
-                selectBox[3].style.display = 'none';
 
-                this.eventListeners();
+
+            this.getTotalCount();
+            this.getBodyCount();
+            this.getDiameter();
+            this.getRingsCount();
+            this.getBodyBottom();
+            this.eventListeners();
             
-
+                
+                
                 }
 
-                constCheckbox() {
+
+                
+
+                
+
+                getBodyCount() {
                     if(myonoffswitch.checked) {
-                    console.log('ОДНОКАМЕРНЫЙ');
+                    this.bodyCount = 10000;
                     titleText[1].style.display = 'none';
                     selectBox[2].style.display = 'none';
                     selectBox[3].style.display = 'none';
-                    //myonoffswitch.removeEventListener('change', this.constCheckbox);
+  
+                    } else {
+                        this.bodyCount = 15000;
+                        titleText[1].style.display = 'block';
+                        selectBox[2].style.display = 'inline-block';
+                        selectBox[3].style.display = 'inline-block';
+                    }
+                    
+                }
+
+
+                getDiameter() {
+
+
+
+                    if(formControl[0]) {
+    
+                        if(formControl[0].value === '1.4 метра'){
+                            this.bodyDiameter = 0;
+
+                        } else if (formControl[0].value === '2 метра'){
+                            const diameterPersentOne = Math.floor(this.bodyCount * 0.2);
+                            this.bodyDiameter = diameterPersentOne;
+                            console.log(this.bodyDiameter);
+                            
+                        }
+                    }
+
+                    if(formControl[2]) {
+                        if(formControl[2].value === '1.4 метра') {
+                            this.bodyDiameter = 0;
+                        } else if (formControl[2].value === '2 метра') {
+                            const diameterPersentTwo = Math.floor(this.bodyCount * 0.3);
+                            this.bodyDiameter = diameterPersentTwo;
+                            console.log(this.bodyDiameter);
+                        }
+                    }
+
+                }
+
+                getRingsCount() {
+
+                    if(formControl[1]) {
+                        if(formControl[1].value === '1 штука') {
+                            this.ringsCount = 0;
+
+                        } else if(formControl[1].value === '2 штуки') {
+                            const ring30PercentOne = Math.floor(this.totalCount * 0.3);
+                            this.ringsCount = ring30PercentOne;
+
+                            } else if(formControl[1].value === '3 штуки') {
+                                const ring50PercentOne = Math.floor(this.totalCount * 0.5);
+                                this.ringsCount = ring50PercentOne;
+                            }
+                    }
+
+                    if(formControl[3]) {
+                        if(formControl[3].value === '1 штука') {
+                            this.ringsCount = 0;
+                            
+                        } else if(formControl[3].value === '2 штуки') {
+                            const ring30PercentTwo = Math.floor(this.totalCount * 0.3);
+                            this.ringsCount = ring30PercentTwo;
+
+                            } else if(formControl[3].value === '3 штуки') {
+                                const ring50PercentTwo = Math.floor(this.totalCount * 0.5);
+                                this.ringsCount = ring50PercentTwo;
+                            }
+                    }
+
+                
+                }
+
+                getBodyBottom() {
+
+                    if(myonoffswitch.checked) {
+                        if (myonoffswitchTwo.checked) {
+                            this.bodyBottom = 1000;
+                        } else {
+                            this.bodyBottom = 0;
+                            }
                         
                         } else {
-                    console.log('ДВУХКАМЕРНЫЙ');
-                    titleText[1].style.display = 'block';
-                    selectBox[2].style.display = 'inline-block';
-                    selectBox[3].style.display = 'inline-block';
-
-                    //myonoffswitch.removeEventListener('change', this.constCheckbox);
-                    }
-                    
+                            if (myonoffswitchTwo.checked) {
+                                this.bodyBottom = 2000;
+                            } else {
+                                this.bodyBottom = 0;                                        
+                            }
+                        }    
                 }
 
-
-                constCheckboxTwo() {
-                    if(myonoffswitchTwo.checked) {
-                        console.log('С ДНОМ');
-                    } else {
-                        console.log('БЕЗ ДНА');
-                    }
-                    
-                }
-
+                
                 eventListeners() {
-                    myonoffswitch.addEventListener('change', this.constCheckbox);
-                    myonoffswitchTwo.addEventListener('change', this.constCheckboxTwo);
-              
+                    
+                    accordion.addEventListener('change', () => {
+                    this.getBodyCount();
+                    this.getDiameter();
+                    this.getRingsCount();
+                    this.getBodyBottom();
+                    this.getTotalCount();
+                    
+                    
+                    });
+          
+                    
+                }
+
+                getTotalCount() {
+                    this.totalCount = this.bodyCount + this.bodyDiameter + this.ringsCount + this.bodyBottom;
+                    calcResult.value = this.totalCount; 
+                     
                 }
             }
 
@@ -132,14 +238,12 @@ window.addEventListener('DOMContentLoaded', () => {
         };
 
         septCalculator();
-
-
-
-
-        
+    
     };
 
     septConstructor();
+
+    
 
 });
 
